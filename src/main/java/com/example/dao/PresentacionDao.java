ac664f63692816e886675ce7d09c6ab2f1d197f5
bymapi <mapihernandezc@gmail.com>
2024-02-19 12:32:22 +0100

    Dao finish -> Begin Controllers

--- src/main/java/com/example/dao/PresentacionDao.java
+++ src/main/java/com/example/dao/PresentacionDao.java
@@ -1,5 +1,31 @@
 package com.example.dao;
 
-public interface PresentacionDao {
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.entities.Presentacion;
+
+@Repository
+public interface PresentacionDao extends JpaRepository<Presentacion,Integer> {
+
+    // Paginacion 
+    // 1-Sacar los productos asociados a una presentacion 
+    @Query(value = "select p from Presentacion p left join fetch p.productos",
+    countQuery = "select count(p) from Presentacion p left join p.productos")
+
+    public Page<Presentacion> findAll(Pageable pageable);
+
+    @Query(value = "select p from Presentacion p left join fetch p.productos")
+    public List<Presentacion> findAll(Sort sort);
+
+    // Metodo 3
+    @Query(value = "select p from Presentacion p left join fetch p.productos where p.id = :id")
+    public Presentacion findById(int id);
 
 }
--- src/main/java/com/example/dao/ProductoDao.java
+++ src/main/java/com/example/dao/ProductoDao.java
@@ -1,5 +1,36 @@
 package com.example.dao;
 
-public interface ProductoDao {
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+import com.example.entities.Producto;
+
+@Repository
+public interface ProductoDao extends JpaRepository<Producto,Integer>{
+
+    @Query(value = "select p from Producto p left join fetch p.presentaciones",
+    countQuery = "select count(p) from Producto p left join p.presentaciones")
+    public Page<Producto> findAll(Pageable pageable);
+
+    // Método que ordena los asistentes sin paginación 
+
+    @Query(value = "select p from Producto p left join fetch p.presentaciones")
+    public List<Producto> findAll(Sort sort);
+
+    // Método que recupera el asistente con su presentaciono
+
+    @Query(value = "select p from Producto p left join fetch p.presentaciones where p.id = :id")
+    public Producto findById(int id);
+
+
+
 
 }
